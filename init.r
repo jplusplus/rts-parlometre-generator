ae7955ef49f7a1014076478c52b6355bbad76a12
pirhoo <hello@pirhoo.com>
2015-07-02 10:43:24 +0200

    Added hashbang

--- init.r
+++ init.r
@@ -1,14 +1,6 @@
-#
-# Example R code to install packages
-# See http://cran.r-project.org/doc/manuals/R-admin.html#Installing-packages for details
-#
-
-###########################################################
-# Update this line with the R packages to install:
+#!/usr/bin/env Rscript
 
 my_packages = c("fields", "RJSONIO", "Rook")
-
-###########################################################
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
